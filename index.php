82d94c64b4135c0d65d8bdbb89c2dda7698eed65
Cloud <ruben.as532@gmail.com>
2024-05-16 10:43:53 +0200

    Defined steps to solve the problem

--- index.php
+++ index.php
@@ -5,6 +5,19 @@ $matrix = [[2,7,6],
            [4,3,8]];
 
 function isMagicMatrix(array $matrix): bool {
+    if(checkSumRows($matrix) && checkSumColums($matrix) && checkSumDiagonals($matrix)) return true;
+    return false;
+}
+
+function checkSumRows(array $matrix): bool {
+    return false;
+}
+
+function checkSumColumns(array $matrix): bool {
+    return false;
+}
+
+function checkSumDiagonals(array $matrix): bool {
     return false;
 }
 
