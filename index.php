0c2bb721c51537cea505a22c3c6e978cd81198f5
Cloud <ruben.as532@gmail.com>
2024-05-16 11:02:18 +0200

    What I did in 30 minutes

--- index.php
+++ index.php
@@ -21,7 +21,22 @@ function checkSumRows(array $matrix): bool {
 }
 
 function checkSumColums (array $matrix): bool {
-    return false;
+    $column_con = 0;
+    $sum = 0;
+    while($column_con < ARRAY_SIZE) {
+        $row_con = 0;
+        $sum_aux = 0;
+        while($row_con < ARRAY_SIZE) {
+            $sum_aux += $matrix[$column_con][$row_con];
+            ++$row_con;
+        }
+        if($column_con == 0) $sum = $sum_aux;
+        else {
+            if($sum != $sum_aux) return false;
+        }
+        ++$column_con;
+    }
+    return true;
 }
 
 function checkSumDiagonals(array $matrix): bool {
