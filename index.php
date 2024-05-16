93ec694c3afedfe8326af334e6cd9c88e430ef7b
Cloud <ruben.as532@gmail.com>
2024-05-16 10:41:36 +0200

    Main logic of the problem added

--- index.php
+++ index.php
@@ -4,7 +4,14 @@ $matrix = [[2,7,6],
            [9,5,1],
            [4,3,8]];
 
-foreach($matrix as $row) {
-    print_r($row);
+function isMagicMatrix(array $matrix): bool {
+    return false;
+}
+
+if(isMagicMatrix($matrix)) {
+    echo "És una matriu màgica!";
+}
+else {
+    echo "NO és una matriu màgica!";
 }
 ?>
